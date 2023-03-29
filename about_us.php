493a6854f85f01ca9389e7aa20018cb7cdb9b37c
virsingvasava <virsing.vasava255@gmail.com>
2023-03-30 00:38:37 +0530

    about us page make changes fixed

--- about_us.php
+++ about_us.php
@@ -1,7 +1,4 @@
-<?php
-
-
+<?php 
 echo "We are developing the  software and we are provideing serveices and client is very satify with our working flow";
 echo "We have more  ten thousound plus happy client and they have give feedback, you can see there feedback on our  website, you can visit our side. so you can easy identfy our work culture and  developer";
-
 ?>
